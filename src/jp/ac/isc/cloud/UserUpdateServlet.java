3375e0c5de20c16e90b7fb233c53b73d32a5da6c
n1200179 <carn1200179@gn.iwasaki.ac.jp>
2021-08-07 15:38:00 +0900

    練習 02_2

--- src/jp/ac/isc/cloud/UserUpdateServlet.java
+++ src/jp/ac/isc/cloud/UserUpdateServlet.java
@@ -8,17 +8,16 @@ import javax.servlet.annotation.*;
 import javax.servlet.http.*;
 
 /**
- * Servlet implementation class UserInsertServlet
+ * Servlet implementation class UserUpdateServlet
  */
-@WebServlet("/UserInsertServlet")
-public class UserInsertServlet extends HttpServlet {
+@WebServlet("/UserUpdateServlet")
+public class UserUpdateServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
@@ -26,22 +25,24 @@ public class UserInsertServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		try {
 			Connection users = null;
 			try {
 				request.setCharacterEncoding("utf-8");
 				Class.forName("com.mysql.jdbc.Driver");
-				users = DriverManager.getConnection(
-						"jdbc:mysql://localhost/servlet_db?useUnicode=true&characterEncoding=utf8", "root", "");
-				String id = request.getParameter("insertId");
-				String name = request.getParameter("insertName");
-				String picture = request.getParameter("insertPicture");
+				users = DriverManager.getConnection("jdbc:mysql://localhost/servlet_db?useUnicode=true&characterEncoding=utf8", "root", "");
+				String id = request.getParameter("updateId");
+				String name = request.getParameter("updateName");
+				String picture = request.getParameter("updatePicture");
 				Statement state = users.createStatement();
-				state.executeUpdate("INSERT INTO user_table VALUE('" + id + "','" + name +
-						"','" + picture + "')");
+				if(name.length() != 0){
+				state.executeUpdate("UPDATE user_table SET name='" + name  + "' WHERE id ='" + id + "'");
+			}
+				if(picture.length() != 0){
+					state.executeUpdate("UPDATE user_table SET picture='" + picture  + "' WHERE id ='" + id + "'");
+				}
 				state.close();
 				users.close();
 				response.sendRedirect("/select"); //UserSelectServletを呼び出す
@@ -52,5 +53,5 @@ public class UserInsertServlet extends HttpServlet {
 			e.printStackTrace();
 		}
 	}
-
 }
+
